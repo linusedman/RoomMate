486db9d46f6d8cf57701859e34f9fd4737b827f9
linusedman <linus.edman33@gmail.com>
2025-09-23 11:21:40 +0200

    Added tables instruments and instrument_types, the admin role (bool) in users as well as new cascade rules to the database to enfoce the foregin key relationships

--- roomMate_DB_basic.sql
+++ roomMate_DB_basic.sql
@@ -6,14 +6,15 @@ CREATE TABLE `rooms` (
   `id` INT PRIMARY KEY,
   `roomname` varchar(255),
   `floor_id` INT NOT NULL,
-  CONSTRAINT fk_rooms_floor FOREIGN KEY (`floor_id`) REFERENCES `floors` (`id`)
+  CONSTRAINT fk_rooms_floor FOREIGN KEY (`floor_id`) REFERENCES `floors` (`id`) ON UPDATE CASCADE
 );
 
 CREATE TABLE `users` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL UNIQUE
+  `email` varchar(255) NOT NULL UNIQUE,
+  `admin` BOOLEAN
 );
 
 CREATE TABLE `bookings` (
@@ -22,21 +23,33 @@ CREATE TABLE `bookings` (
   `room_id` INT NOT NULL,
   `start_time` DATETIME NOT NULL,
   `end_time` DATETIME NOT NULL, 
-  CONSTRAINT fk_bookings_user FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT fk_bookings_room FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`)
+  CONSTRAINT fk_bookings_user FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT fk_bookings_room FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `instrument_types` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `typename` varchar(255) NOT NULL
+);
+
+CREATE TABLE `instruments` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `type_id` INT NOT NULL,
+  `room_id` INT NOT NULL,
+  CONSTRAINT fk_instruments_instrument_types FOREIGN KEY (`type_id`) REFERENCES `instrument_types` (`id`) ON DELETE CASCADE,
+  CONSTRAINT fk_instruments_room FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE
 );
 
 
--- Alternative user table 
--- CREATE TABLE  users  (
---   id  int NOT NULL AUTO_INCREMENT,
---   username  varchar(45) NOT NULL,
---   email  varchar(45) NOT NULL,
---   password  varchar(45) NOT NULL,
---  PRIMARY KEY ( id ),
---  UNIQUE KEY  id_UNIQUE  ( id )
--- )
+-- INSERT INTO users (username, password, email) VALUES ('test', '1', 'test@test.com');
 
+-- INSERT INTO floors (id) VALUES (1);
 
--- ALTER TABLE users
--- ADD COLUMN email VARCHAR(100) NOT NULL UNIQUE AFTER username;
+-- INSERT INTO rooms (id, floor_id) VALUES (2, 1);
+
+-- INSERT INTO instrument_types (typename) VALUES ('centrifuge');
+
+-- INSERT INTO instruments (user_id, room_id) VALUES (1, 1);
+
+-- INSERT INTO bookings (user_id, room_id, start_time, end_time) VALUES (1, 1, '2008-11-11 13:23:44', '2008-11-11 13:24:00');
+-- INSERT INTO bookings (user_id, room_id, start_time, end_time) VALUES (2, 1, '2008-11-11 13:23:44', '2008-11-11 13:24:00');
