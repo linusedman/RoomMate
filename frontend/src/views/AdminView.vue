dc29b1b0200b732555d6b54fc4dc8f57c971487a
MSL-001 <mattias@levingotland.se>
2025-09-30 16:42:02 +0200

    Added Shell for AdminView

---
+++ frontend/src/views/AdminView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="admin-view d-flex">
+    <div class="left-panel p-3">
+      <AdminUser/>
+      <AdminInstruments/>
+    </div>
+
+    <div class="right-panel p-3">
+      <AdminBookings/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import AdminUser from "../components/AdminUser.vue";
+import AdminBookings from "../components/AdminBookings.vue";
+import AdminInstruments from "../components/AdminInstruments.vue";
+</script>
+
+<style scoped>
+.admin-view { display:flex; gap:1rem; }
+.left-panel, .right-panel { width:50%; }
+</style>
