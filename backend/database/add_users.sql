fe0e8653ca30d5a360a065252abfa448a57ef166
linusedman <linus.edman33@gmail.com>
2025-10-07 10:38:55 +0200

    Deleted file add_users.sql since it is not compatible with the password hashing

--- backend/database/add_users.sql
+++
@@ -1,8 +0,0 @@
--- The hashed password won't work if wee create users this way!
-
-INSERT INTO users (username, password, email)
-VALUES
-('Tom_Sawyer', 'Password1', 'tom@example.com'),
-('Huck_Finn', 'Password2', 'huck@example.com'),
-('Sherlock_Holmes', 'Password3', 'sherlock@example.com'),
-('Oliver_Twist', 'password4', 'oliver@example.com');
