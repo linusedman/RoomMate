5dca4d03c61f33d08fe4e053b113a39e2d7c353a
linusedman <linus.edman33@gmail.com>
2025-10-10 13:16:21 +0200

    Added delete account button (not working yet)

--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -1,6 +1,12 @@
 <template>
-    <h1>
-        Settings
-    </h1>
-
+    <h1>Settings</h1>
+    <div class="danger-card">
+      <button class="delete-btn" @click="deleteAccount">
+        Delete My Account
+      </button>
+      <div class="warning-text">
+        This action is irreversible.
+      </div>
+    </div>
 </template>
+
